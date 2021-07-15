ea47d712b923bf0907e9571397a740f82d2e1ecd
Vedran Alagić <59015187+valagic@users.noreply.github.com>
2021-07-15 18:21:56 +0200

    kreiranje projekta

---
+++ Nba/src/module-info.java
@@ -0,0 +1,3 @@
+module nba {
+	requires java.desktop;
+}
---
+++ Nba/src/nba/Start.java
@@ -0,0 +1,11 @@
+package nba;
+
+public class Start {
+
+	public Start() {
+		
+	}
+	public static void main(String[] args) {
+		new Start();
+	}
+}
